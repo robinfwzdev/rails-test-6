2c74d805d978cd7901dbd0067bf9c060f518903f
robinfwzdev <robin@futureworkz.com>
2016-01-16 12:55:15 +0700

    Add association mother to son

--- app/models/son.rb
+++ app/models/son.rb
@@ -1,3 +1,4 @@
 class Son < User
   belongs_to :father, :class_name => 'Father', :foreign_key => 'user_id'
+  belongs_to :mother, :class_name => 'Mother', :foreign_key => 'user_id'
 end
--- spec/models/son_spec.rb
+++ spec/models/son_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Son do
   context 'associations' do
     it { is_expected.to belong_to :father }
+    it { is_expected.to belong_to :mother }
   end
 end
