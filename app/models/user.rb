6e6d810d8d2cfee4f8d02e8408fe1e4b2f41f143
robinfwzdev <robin@futureworkz.com>
2016-01-16 10:33:25 +0700

    validate age for person

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :dob,  presence: true
+
+  MIN_AGE = 0
+
+  def age
+    ((Date.today - dob) / 365).floor
+  end
+
+  protected
+
+  def ensure_valid_age
+    return errors[:dob] << 'Invalid.' if dob? && age < self.class::MIN_AGE
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,30 @@ describe User do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :dob }
   end
+
+  before(:each) do
+    allow(Date).to receive(:today).and_return('15/1/2015'.to_date)
+  end
+
+  describe '#ensure_valid_age?' do
+    let(:user) { build(:user) }
+
+    context 'valid' do
+      ['06/04/1990', '06/10/2000'].each do |dob|
+        it "#{dob}" do
+          user.dob = dob
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    context 'invalid' do
+      ['06/04/2500', '06/10/2025'].each do |dob|
+        it "#{dob}" do
+          user.dob = dob
+          expect(user).to_not be_valid
+        end
+      end
+    end
+  end
 end
