32e0e290a7660bc7e0ce74d1e1cc158fcf175d30
robinfwzdev <robin@futureworkz.com>
2016-01-16 13:20:03 +0700

    Change association on son model

--- app/models/son.rb
+++ app/models/son.rb
@@ -1,4 +1,4 @@
 class Son < User
-  belongs_to :father, :class_name => 'Father', :foreign_key => 'user_id'
-  belongs_to :mother, :class_name => 'Mother', :foreign_key => 'user_id'
+  belongs_to :father, :class_name => 'Father', :foreign_key => 'father_id'
+  belongs_to :mother, :class_name => 'Mother', :foreign_key => 'mother_id'
 end
