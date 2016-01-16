cfdbfdf627754680d6c19b2f532460feefcd5939
robinfwzdev <robin@futureworkz.com>
2016-01-16 12:34:19 +0700

    Add user_id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116034607) do
+ActiveRecord::Schema.define(version: 20160116052148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,11 @@ ActiveRecord::Schema.define(version: 20160116034607) do
   add_index "father_infos", ["father_id"], name: "index_father_infos_on_father_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "gender"
-    t.string "type"
-    t.date   "dob"
+    t.string  "name"
+    t.string  "gender"
+    t.string  "type"
+    t.date    "dob"
+    t.integer "user_id"
   end
 
 end
