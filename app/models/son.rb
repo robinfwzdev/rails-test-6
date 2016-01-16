71adc82a29664482b1b807c26304a87c93aaa8f2
robinfwzdev <robin@futureworkz.com>
2016-01-16 14:43:48 +0700

    Remove parents method on son model

--- app/models/son.rb
+++ app/models/son.rb
@@ -1,10 +1,4 @@
 class Son < User
   belongs_to :father, :class_name => 'Father', :foreign_key => 'father_id'
   belongs_to :mother, :class_name => 'Mother', :foreign_key => 'mother_id'
-
-  def parents
-    parents = Array.new
-    parents.push(father)
-    parents.push(mother)
-  end
 end
