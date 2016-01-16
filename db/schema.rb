74a9cf7b8b5c78b1707302278d9aafd11f3589dc
robinfwzdev <robin@futureworkz.com>
2016-01-16 10:24:23 +0700

    Add dob to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116024150) do
+ActiveRecord::Schema.define(version: 20160116032308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160116024150) do
     t.string "name"
     t.string "gender"
     t.string "type"
+    t.date   "dob"
   end
 
 end
