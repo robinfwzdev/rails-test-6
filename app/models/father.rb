72282487915cebab48ec7523509ae1359dfb7395
robinfwzdev <robin@futureworkz.com>
2016-01-16 13:26:20 +0700

    change user_id to father_id on father model

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,6 +1,6 @@
 class Father < User
-  has_many :sons, :class_name => 'Son', :foreign_key => 'user_id'
-  has_many :daughters, :class_name => 'Daughter', :foreign_key => 'user_id'
+  has_many :sons, :class_name => 'Son', :foreign_key => 'father_id'
+  has_many :daughters, :class_name => 'Daughter', :foreign_key => 'father_id'
 
   MIN_AGE = 20
 end
