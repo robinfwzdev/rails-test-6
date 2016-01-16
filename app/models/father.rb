728d95fd5ace4681389884a92d24a81425d48846
robinfwzdev <robin@futureworkz.com>
2016-01-16 15:35:01 +0700

    Remove association on father model

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,6 +1,3 @@
 class Father < User
-  has_many :sons, :class_name => 'Son', :foreign_key => 'father_id'
-  has_many :daughters, :class_name => 'Daughter', :foreign_key => 'father_id'
-
   MIN_AGE = 20
 end
