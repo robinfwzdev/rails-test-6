46c94595809566d6ba939861db9e3a7b69c2943a
robinfwzdev <robin@futureworkz.com>
2016-01-16 16:03:10 +0700

    remove daughter model

--- app/models/daughter.rb
+++
@@ -1,4 +0,0 @@
-class Daughter < User
-  belongs_to :father, :class_name => 'Father', :foreign_key => 'father_id'
-  belongs_to :mother, :class_name => 'Mother', :foreign_key => 'mother_id'
-end
--- spec/models/daughter_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Daughter do
-  context 'associations' do
-    it { is_expected.to belong_to :father }
-    it { is_expected.to belong_to :mother }
-  end
-end
