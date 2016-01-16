464f2072a80780c421829ade49a387afe313e031
robinfwzdev <robin@futureworkz.com>
2016-01-16 10:26:09 +0700

    Validate name and dob for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe User do
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :dob }
   end
 end
